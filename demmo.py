bd3b7a09a9fc996fb0a80f8777630be6f9bc2552
fcbba <fcbmanoj1548@gmail.com>
2020-11-04 19:31:33 +0530

    added

--- demmo.py
+++ demmo.py
@@ -1,1 +1,10 @@
-print("hello")
+import cv2
+video = cv2.VideoCapture(0);
+while(True):
+    ret,frame = video.read()
+    cv2.imshow('fram',frame)
+
+    if cv2.waitKey(1) == ord('m'):
+        break
+video.release()
+cv2.destroyAllWindows()
